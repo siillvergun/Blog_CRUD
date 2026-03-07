78ba4feca8e9639b64c9c8986d4b8a1668b33e58
siillvergun <duejql03@naver.com>
2026-03-07 21:24:15 +0900

    jwt의존성 선정 및 추가

--- src/main/java/com/siillvergun/blog/auth/jwt/JwtUtil.java
+++ src/main/java/com/siillvergun/blog/auth/jwt/JwtUtil.java
@@ -1,4 +1,13 @@
 package com.siillvergun.blog.auth.jwt;
 
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+    // 토큰 발급
+
+    // 토큰 검증
+
+    // 토큰에서 사용자 정보 꺼내기
 }
